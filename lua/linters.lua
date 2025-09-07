f01a7b7d7454e24c8112753d5a0d63416feb1498
Nathanael <Shakadak@users.noreply.github.com>
2025-09-07 23:08:13 +0200

    linters update

--- lua/linters.lua
+++ lua/linters.lua
@@ -1,7 +1,15 @@
+-- https://github.com/mantoni/eslint_d.js?tab=readme-ov-file#neovim
+-- unproven to work yet
+vim.env.ESLINT_D_PPID = vim.fn.getpid()
+
+-- https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#usage
 require('lint').linters_by_ft = {
   elixir = {'credo'},
+  javascript = {'eslint_d'},
+  typescript = {'eslint_d'},
 }
 
+-- https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#usage
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
 
